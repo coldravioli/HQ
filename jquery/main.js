a5fdfd713f516d8723dece7a813b108d6ed4e4e1
Raya <125909608+coldravioli@users.noreply.github.com>
2024-10-29 18:17:48 -0400

    ADD SO MUCH

---
+++ jquery/main.js
@@ -0,0 +1,20 @@
+$(".dark-btn").click(
+    function(){
+    $("body").toggleClass("dark");
+    $(".box").toggleClass("dark");
+  });
+
+  $(".spin-btn").click(
+    function(){
+        $(".box").toggleClass("spin");
+
+  });
+
+  $(".reveal-btn").click(
+    function(){
+        $(".shark").addClass("reveal");
+        // $(".reveal-btn").css("display", "none");
+        $(".reveal-btn").hide();
+  });
+
+  $(".draggable").draggable();
